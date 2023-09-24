194fb088060e7de9fd50f95dad5e7ef689e0f308
yubin-im <140530127+yubin-im@users.noreply.github.com>
2023-09-24 21:52:23 +0900

    [level 1] Title: 체육복, Time: 0.37 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+class Solution {
+    public static int solution(int n, int[] lost, int[] reserve) {
+        int answer = n - lost.length;
+
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+
+        // 여벌 체육복을 가져온 학생이 도단당했을때
+        for(int i = 0; i < lost.length; i++) {
+            for(int j = 0; j < reserve.length; j++) {
+                if (lost[i] == reserve[j]) {
+                    answer++;
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+
+        // 도난당한 학생에게 빌려줄때 
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if ((lost[i] - 1 == reserve[j]) || lost[i] + 1 == reserve[j]) {
+                    answer++;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
