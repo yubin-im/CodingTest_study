abcb37e236d436743a0269e5a497763de0e7a330
임유빈 <140530127+yubin-im@users.noreply.github.com>
2024-08-23 13:24:21 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 3.36 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        
+        for(int i = left; i <= right; i++) {
+            int count = 0;
+            
+            for(int j = 1; j <= i; j++) {
+                if(i % j == 0) {
+                    count += 1;
+                }
+            }
+            if(count % 2 == 0) {
+                answer += i;
+            } else {
+                answer -= i;
+            }
+        }
+    
+        return answer;
+    }
+}
