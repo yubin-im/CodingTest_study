0e0493d9608b9384dc5dc6d90d6e9ec59a56d0bc
임유빈 <140530127+yubin-im@users.noreply.github.com>
2024-09-12 13:01:16 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        int len = number.length;
+        
+        for(int i = 0; i < len; i++) {
+            for(int j = i + 1; j < len; j++) {
+                for(int k = j + 1; k < len; k++) {
+                    if(number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
